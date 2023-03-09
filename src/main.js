92b402e71ff867a67a869dbd5bccc8d47c4ade1b
thunnuht <77015864+thunyathep@users.noreply.github.com>
2023-03-09 13:04:12 +0700

    Update : install pinia and  axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+
+const pinia = createPinia()
 
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
