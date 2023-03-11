068f20e2e441fe4ff738b1cb0ff83b18e73fbbe1
Surawee <surawee.1399200001403@gmail.com>
2023-03-12 00:46:22 +0700

    Set Up Firebase

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,14 +1,46 @@
 <template>
-    <main class="post-page">
-        <h1>Post</h1>
-        <p>This is the post page</p>
-    </main>
+  <main class="post-page">
+    <h1>Post</h1>
+    <p>This is the post page</p>
+    <row>
+      <button
+        class="btn btn-primary"
+        data-bs-target="#collapseTarget1"
+        data-bs-toggle="collapse"
+      >
+        ผลรวมของการโพสหต์ทั้งหมด
+      </button>
+      &nbsp;&nbsp;&nbsp;
+      <button
+        class="btn btn-primary mr-1"
+        data-bs-target="#collapseTarget2"
+        data-bs-toggle="collapse"
+      >
+        ปัญหาด้านความการเรียน
+      </button>
+      &nbsp;&nbsp;&nbsp;
+      <button
+        class="btn btn-primary mr-1"
+        data-bs-target="#collapseTarget2"
+        data-bs-toggle="collapse"
+      >
+        ปัญหาด้านครอบครัว
+      </button>
+      &nbsp;&nbsp;&nbsp;
+      <button
+        class="btn btn-primary mr-1"
+        data-bs-target="#collapseTarget2"
+        data-bs-toggle="collapse"
+      >
+        ปัญหาด้านความรัก
+      </button>
+      <!-- <div class="collapse py-2" id="collapseTarget2">
+        This is the toggle-able content!
+      </div> -->
+    </row>
+  </main>
 </template>
 
-<script>
+<script></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
