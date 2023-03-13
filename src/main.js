e5e76a81d2db4548d161cf22c62e29e8e58eeb8b
Surawee <surawee.1399200001403@gmail.com>
2023-03-13 16:37:29 +0700

    งง1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './Firebase.js'
+//import './Firebase.js'
 import router from './router'
-import BootstrapVue3 from 'bootstrap-vue-3'
+// import BootstrapVue3 from 'bootstrap-vue-3'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 //app.use(BootstrapVue3)
 createApp(App).use(router).use(BootstrapVue3).mount('#app')
