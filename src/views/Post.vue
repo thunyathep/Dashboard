bdd54134b2ee65d73977c95666b52c7d4212f457
Surawee <surawee.1399200001403@gmail.com>
2023-03-13 17:16:47 +0700

    อย่าพึ่งตุย

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,14 +1,25 @@
 <template>
-    <main class="post-page">
-        <h1>Post</h1>
-        <p>This is the post page</p>
-    </main>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage1">Click me 1</button>
+    <button @click="changeMessage2">Click me 2</button>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      message: 'Hello, world!'
+    }
+  },
+  methods: {
+    changeMessage1() {
+      this.message = 'Hi there!'
+    },
+    changeMessage2() {
+      this.message = 'Button 2 clicked!'
+    }
+  }
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
